ef4dc3db184e7d8d05bf2186d425115f5c426733
Andres Marquez <andresmr955@gmail.com>
2025-03-11 11:41:36 -0400

    adding files

---
+++ 2.INTEGERSFLOATSBOOLEANS/operations.py
@@ -0,0 +1,10 @@
+#Arithmetic Operators
+a = 10
+b = 3
+print("Addition: " , a + b)
+print("Subtraction: " , a - b)
+print("Multiplication: " , a * b)
+print("Division: " , a/b) #You have to put attention with any division by 0
+print("Modulus : " , a % b)
+print("Exponentiation  : " , a ** b)
+print("Floor division  : " , a // b)
