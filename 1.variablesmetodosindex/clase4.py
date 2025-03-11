3e318c332babb45847fca6b3a3cbece87ac50f2b
Andres Marquez <andresmr955@gmail.com>
2025-03-11 11:22:28 -0400

    Learning the fundamentals

---
+++ 1.variablesmetodosindex/clase4.py
@@ -0,0 +1,24 @@
+
+name_comillas_Simples = ' David '
+name_comillas_Dobles = "Andres"
+name_comillas_triples = '''Caicedo
+
+
+Marquez
+'''
+
+print(type(name_comillas_Simples))
+print(type(name_comillas_Dobles))
+print(type(name_comillas_triples))
+print(name_comillas_Simples)
+print(name_comillas_Dobles)
+print(name_comillas_triples)
+print(name_comillas_Simples[0])
+print(name_comillas_Simples[1])
+print(name_comillas_Simples[2])
+print(name_comillas_Simples[-2] + " Contar para atras")
+print(name_comillas_Simples + " " + name_comillas_Dobles + " Concatenando con espacio")
+print(len(name_comillas_Simples))
+print(name_comillas_Simples.lower())
+print(name_comillas_Simples.upper())
+print(name_comillas_Simples.strip()) #Eliminar espacios del comienzo y final
---
+++ 1.variablesmetodosindex/intFloatBool.py
@@ -0,0 +1,15 @@
+x = 10
+print(type(x))
+y = 9.9874
+print(type(y))
+z = 1.2E6
+a = 1.2E-6
+print(type(z))
+print(z)
+print(type(a))
+print(a)
+print(x+y)
+is_true = True #Write it with upper Case
+is_False = False
+print(type(is_true))
+
