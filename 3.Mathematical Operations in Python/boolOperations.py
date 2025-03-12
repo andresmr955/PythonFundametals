fd80944fef652ad35cf285c17d9c313046d9ec4b
Andres Marquez <andresmr955@gmail.com>
2025-03-12 08:03:06 -0400

    adding more clases

---
+++ 3.Mathematical Operations in Python/boolOperations.py
@@ -0,0 +1,8 @@
+a = 10
+b = 10.0
+
+print(f'a {a} Smaller than b {b}: {a < b}')
+print(f'a {a} Greater than or equal to b {b}: {a >= b}')
+print(f'a {a} Smaller than or equal to b {b}: {a <= b}')
+print(f'a {a} Equal to b {b}: {a == b}')
+print(f'a {a} Not equal to b {b}: {a != b}')
---
+++ 4.IOconcoleoperations/inputyoutput.py
@@ -0,0 +1,11 @@
+import os
+
+##Input/Output Console Operations
+name = input(f'What is your name => ')
+print(f'Helo my name is Andres and I am your IA, nice to meet you {name}')
+age = int(input(f'I would like to know, how old are you? => '))
+ageFloat = float(3.14168)
+print(f'''You are {age} years old, ohh !!! you are still young, 
+      that is the right time to start to code. The type of your age is {type(age)}, 
+      but if it were a float like PI {ageFloat}, it will be a {type(ageFloat)}, 
+      with this explanation hope you do not have gaps in your knowdledge''')
