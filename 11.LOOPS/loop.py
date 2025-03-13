1ea18ccce1b9c13a9bcc085712f6ad9b792ca379
Andres Marquez <andresmr955@gmail.com>
2025-03-13 08:18:12 -0400

    Adding loops to my knowledge

---
+++ 11.LOOPS/loop.py
@@ -0,0 +1,14 @@
+numbers = [1, 2, 3, 4, 5, 6]
+for i in numbers:
+    print("Here is equal to: ", i+1)
+for i in range(3,10):
+    print(i)
+
+fruits = ["Apple", "Pear", "Grape", "Orange","Tomato"]
+for fruit in fruits:
+    print(fruit)
+    if fruit == "Orange":
+        print("Orange found it")
+
+
+#WHILE 
