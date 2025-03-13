50d2fcc6fbda14f7158d4c71d97aa672bf0c0a70
Andres Marquez <andresmr955@gmail.com>
2025-03-13 08:26:54 -0400

    Adding loops to my knowledge 1.0

--- 11.LOOPS/loop.py
+++ 11.LOOPS/loop.py
@@ -12,3 +12,22 @@ for fruit in fruits:
 
 
 #WHILE 
+x = 0
+while x<=5:
+    
+    print(x)
+    x +=1  #x = x +1
+
+#BREAK    
+x = 0
+while x<5:
+    if x ==3:
+        break
+    print(x)
+    x +=1  #x = x +1 
+#Skip
+numbers = [1, 2, 3, 4, 5, 6]
+for i in numbers:
+    if i == 3:
+        continue
+    print("Here i is equal to: ", i)
