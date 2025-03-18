716ef12201af65cc5681d59e72bbb9eb65c1d298
Andres Marquez <andresmr955@gmail.com>
2025-03-18 18:20:59 -0400

    Adding the project 8 of platzi

---
+++ 21DaysPython/c_leapyear.py
@@ -0,0 +1,8 @@
+def is_leap_year(year):
+        
+    if year % 4 == 0:
+        if  and year  % 400 == 0 and year % 100 == 0 and year % 400 == 0:
+    else: 
+        return False        
+print(is_leap_year(2004))
+year > 0
