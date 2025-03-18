d073eebd2a09afc9826b3b61053371660d6c9688
Andres Marquez <andresmr955@gmail.com>
2025-03-18 18:45:22 -0400

    This is the leapyaer project

--- 21DaysPython/c_leapyear.py
+++ 21DaysPython/c_leapyear.py
@@ -1,8 +1,26 @@
+'''
+In this Python challenge, you must create the logic for the is_leap_year function, which determines whether a year is a leap year or not. A year is a leap year if it meets the following conditions:
+
+It is divisible by 4, but not by 100.
+If it is divisible by 100 it must be divisible by 400 as well.
+The is_leap_year function receives a single parameter: the year to evaluate. It must return True if the year is a leap year or False otherwise.
+
+Note that the function must be able to handle non-integer or negative values.
+
+Translated with DeepL.com (free version)
+'''
 def is_leap_year(year):
-        
-    if year % 4 == 0:
-        if  and year  % 400 == 0 and year % 100 == 0 and year % 400 == 0:
-    else: 
-        return False        
-print(is_leap_year(2004))
-year > 0
+    # tu código aquí 👈
+    if year % 100  == 0 and year % 400 == 0:
+        return True
+    elif year % 4 == 0:
+        if year % 100 == 0:
+            return False
+        else:
+            if year >= 0:
+                return True
+            else:
+                return False
+    else:
+        return False         
+
