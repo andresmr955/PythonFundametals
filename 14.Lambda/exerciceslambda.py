3dd6410a1037d0d10b42e3045dcf5ebd6a8d4d8d
Andres Marquez <andresmr955@gmail.com>
2025-03-21 16:18:01 -0400

    practicing with filetr, uper and len with lambda

--- 14.Lambda/exerciceslambda.py
+++ 14.Lambda/exerciceslambda.py
@@ -2,7 +2,16 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 filtering_numbers_impairs = filter(lambda a : a % 2 != 0, numbers)
 
-squared_odd = list(map(lambda b: b ** 2, filtering_numbers_impairs))
+squared_ = list(map(lambda b: b ** 2, filtering_numbers_impairs))
 
 
-print(squared_odd)
+print(squared_)
+
+
+names = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank"]
+
+names_with_more_five = filter(lambda a: len(a) >= 5 ,names)
+
+upper_names = list(map(lambda b: b.upper(), names_with_more_five))
+
+print(upper_names)
