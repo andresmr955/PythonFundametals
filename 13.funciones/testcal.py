eef4090a0cdce012228dfe6809b19c5364007c43
Andres Marquez <andresmr955@gmail.com>
2025-03-21 14:31:14 -0400

    Calculator created

---
+++ 13.funciones/testcal.py
@@ -0,0 +1,15 @@
+
+
+def input_User():
+    user_choice = input('''
+    Please chose which operation you want to do:
+    1. Addition
+    2. Subtraction
+    3. Multiplication
+    4. Division
+    5. Exit        
+    
+''')
+    return user_choice
+
+resultado = input_User()    
