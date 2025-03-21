4b220c41461921e22a348a85d2e289da4b099a26
Andres Marquez <andresmr955@gmail.com>
2025-03-21 15:58:20 -0400

    odd numbers using lambda and method filter and map

---
+++ 14.Lambda/lambda.py
@@ -0,0 +1,8 @@
+add = lambda a, b: a + b
+subtraction = lambda a, b: a + b
+multiply = lambda a , b: a * b
+
+##square number
+numbers = range(11)
+squared_numbers = list(map(lambda x: x**2, numbers ))
+print(squared_numbers)
