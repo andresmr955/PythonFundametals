241d3e23cabc47c1ffbce314bbec4eaac67e3b12
Andres Marquez <andresmr955@gmail.com>
2025-03-21 15:36:41 -0400

    Doing modules and making just one pakcet

---
+++ 13.funciones/Calculator_Project/calculator/operations.py
@@ -0,0 +1,11 @@
+def addition(a, b):
+            return a + b 
+def subtraction(a, b):
+        return a - b 
+def multiply(a, b):
+        return a * b 
+def divide(a, b):
+    if b == 0:
+        raise ValueError("It is not possible to divide by 0")
+    else: 
+        return a / b     
