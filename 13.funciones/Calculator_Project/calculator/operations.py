6c9e58ae26ba2b8d07038fa6040cf67589ba4fce
Andres Marquez <andresmr955@gmail.com>
2025-03-21 15:44:13 -0400

    updating regular functions to lambdas

--- 13.funciones/Calculator_Project/calculator/operations.py
+++ 13.funciones/Calculator_Project/calculator/operations.py
@@ -1,9 +1,8 @@
-def addition(a, b):
-            return a + b 
-def subtraction(a, b):
-        return a - b 
-def multiply(a, b):
-        return a * b 
+addition = lambda a,b: a + b 
+subtraction = lambda a, b: a - b
+multiply = lambda a , b: a * b
+
+##The only expression it can not work with lambda is conditional and raise
 def divide(a, b):
     if b == 0:
         raise ValueError("It is not possible to divide by 0")
