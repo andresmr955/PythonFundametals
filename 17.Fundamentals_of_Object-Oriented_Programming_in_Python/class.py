0fea690207e6cb8543d23430a887fa3d35374031
Andres Marquez <andresmr955@gmail.com>
2025-03-25 09:00:53 -0400

    Creating a class called person

--- 17.Fundamentals_of_Object-Oriented_Programming_in_Python/class.py
+++ 17.Fundamentals_of_Object-Oriented_Programming_in_Python/class.py
@@ -1,2 +1,16 @@
 class Person: 
-    def __
+    ##Here we define the attributes
+    #This is the constructor
+    def __init__(self, name, age):
+        self.name = name 
+        self.age = age
+    def greet(self):
+        print(f'Hello, my name is {self.name} and my age is {self.age}')
+    
+
+person_a = Person("Ana", 8)
+person_b = Person("Luis", 25)
+
+#This is the method
+person_a.greet()
+person_b.greet()
