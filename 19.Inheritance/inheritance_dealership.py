d8d36684a5746aff4e2358e0fbfaf72364255c17
Andres Marquez <andresmr955@gmail.com>
2025-03-26 10:55:07 -0400

    Learning, abstraction, polyphormism, inheritance and encapsulation

--- 19.Inheritance/inheritance_dealership.py
+++ 19.Inheritance/inheritance_dealership.py
@@ -10,10 +10,11 @@ class Vehicle:
     def sell(self):
         if self.is_available:
             self.is_available = False
-            print(f'You can sell the car {self.brand}')
+            print(f'The vehicle {self.brand} has been sold')
         else:
             print(f'The vehicle is not available')
 
+##ABSTRACTION
     def check_available(self):
         return self.is_available
     def get_price(self):
@@ -26,8 +27,9 @@ class Vehicle:
 
 
 ##The syntax to say that a subclass inherit from a parent class 
-
+##INHERITANCE
 class Car(Vehicle):
+    #POLYMORPHISM
     def start_engine(self):
         if not self.is_available:
             return f"The car's motor {self.brand} is rolling"
@@ -41,8 +43,96 @@ class Car(Vehicle):
             return f"he car's motor {self.brand} is not available"
         
 
-car1 = Car("chevrolet", 2012, 2500)
+class Bike(Vehicle):
+    def start_engine(self):
+        if not self.is_available:
+            return f"The bike {self.brand} is rolling"
+        else:
+            return f"he bike {self.brand} is not available"
+    
+    def stop_engine(self):
+        if self.is_available:
+            return f"he bike {self.brand} has stopped"
+        else:
+            return f"he bike {self.brand} is not available"
+        
 
-car1.sell()
+class Truck(Vehicle):
+    def start_engine(self):
+        if not self.is_available:
+            return f"The truck's motor {self.brand} is rolling"
+        else:
+            return f"he truck's motor {self.brand} is not available"
+    
+    def stop_engine(self):
+        if self.is_available:
+            return f"he truck's motor {self.brand} has stopped"
+        else:
+            return f"he truck's motor {self.brand} is not available"
+        
 
-car1.start_engine()
+class Customer:
+    def __init__(self, name):
+        self.name = name 
+        self.purchased = []
+
+##This part is important because we used a vehicle class parent
+    def buy_vehicle(self, vehicle: Vehicle):
+        if vehicle.check_available():
+            vehicle.sell()
+            self.purchased.append(vehicle)
+        else:
+            print(f'Sorry this vehicle {vehicle} is not available')
+    
+    def check_available_vehicle(self, vehicle: Vehicle):
+        if vehicle.check_available():
+            availability = "Available"
+        else: 
+            availability = "Not Available"
+        print(f'The vehicle {vehicle.brand} is {availability} and costs {vehicle.get_price()}')
+
+class Dealership:
+    def __init__(self):
+        self.inventory = []
+        self.customers = []
+
+    def add_vehicles(self, vehicle:Vehicle):
+        self.inventory.append(vehicle)
+        print(f'The vehicle {vehicle.brand} has been added to the inventory')
+
+    def register_customer(self, customer: Customer):
+        self.customers.append(customer)
+        print(f'The customer {customer} has been registered')   
+
+    def show_vehicles_available(self):
+        print(f'Vehicles available in the store')
+        for vehicle in self.inventory:
+            if vehicle.check_available():
+                print(f"- {vehicle.brand} and the price is {vehicle.get_price()}")
+
+
+car1 = Car("Toyota", "Corrolla", 2000)
+bike1 =Bike("Yamaha", "MT-09", 7000)
+truck1 = Truck("Volvo", "FH16", 8000)
+
+customer1 = Customer("Carlos")
+
+dealership = Dealership()
+dealership.add_vehicles(car1)
+dealership.add_vehicles(bike1)
+dealership.add_vehicles(truck1)
+
+##Show available vehicles
+
+dealership.show_vehicles_available()
+
+## customer wants to check a vehicle
+
+customer1.check_available_vehicle(car1)
+
+##customer wants to buy a car
+customer1.buy_vehicle(car1)
+
+##Show available vehicles
+
+dealership.show_vehicles_available()
