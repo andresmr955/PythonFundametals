5f2fc6c554836654475d8c7998cc0624f9f5ab8a
Andres Marquez <andresmr955@gmail.com>
2025-03-25 21:01:02 -0400

    making and inheritance and extending and parent class

--- 19.Inheritance/inheritance_dealerchip.py
+++
@@ -1,9 +0,0 @@
-class Vehicule:
-    def __init__(self, brand, model, price):
-        self.brand = brand
-        self.mode = model
-        self.price = price
-        self.is_avalaible = True
-
-#The subclasses child are going to inherit all parameters that we are putting in the super Class
-
