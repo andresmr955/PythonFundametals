9c8344961fce0e9b2300cd46305cdfd4f044f55e
Andres Marquez <andresmr955@gmail.com>
2025-03-27 13:00:47 -0400

    learning library MATH

---
+++ 24.LibraryOSMATHRANDOM/libraryMath.py
@@ -0,0 +1,9 @@
+import math
+
+#Find the area and perimeter (circumference) of a circle.
+
+radius = 5 
+area = math.pi * radius ** 2
+perimeter = 2 * math.pi * radius
+
+print(f'Area: {area}\nPerimeter: {perimeter}')
