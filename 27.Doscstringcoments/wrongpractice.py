6100711987b9f9c41e1fe9b11fc263bb9ced1e08
Andres Marquez <andresmr955@gmail.com>
2025-03-28 09:16:51 -0400

    wrong practices in Docstring

---
+++ 27.Doscstringcoments/wrongpractice.py
@@ -0,0 +1,7 @@
+def calculate_area(base, height):
+
+    """
+    Calculates the area of a triangle
+    """
+    ##Multiply the base times height and then we divide between two
+    return(base * height) / 2
