4c62712445eda7d4c47bbbda034bda547d0a949d
Andres Marquez <andresmr955@gmail.com>
2025-03-28 18:34:47 -0400

    Practicing closures and variables

---
+++ 28.Scope_y_closures/exercisescope.py
@@ -0,0 +1,38 @@
+"""
+crear 2 Funciones
+
+La primera funcion recibira un diccionario que tenga 3 claves nam age and salary
+La segunda funcion debera analizar el salario y  devolver el 
+salario de los empleados que ganen cierto monto
+"""
+
+
+def receive_dic():
+     
+    return  [
+    
+    {
+    "name" : "Carlos",
+    "age" : "25", 
+    "salary" : 4500
+    }, 
+    {
+    "name" : "Camilo",
+    "age" : "75", 
+    "salary" : 8500
+    },
+    {
+    "name" : "Sandra",
+    "age" : "17", 
+    "salary" : 3600
+    }
+]
+
+def filtre_return(empleados, salary):
+        return [empleado for empleado in empleados if empleado["salary"] > salary]
+        
+create_empleados = receive_dic()
+
+empleados_filtrados = filtre_return(create_empleados, 4000)
+
+print(empleados_filtrados)
