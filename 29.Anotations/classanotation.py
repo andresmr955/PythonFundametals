51202a872513b7db2f4064e42c3ef151e011f074
Andres Marquez <andresmr955@gmail.com>
2025-03-28 13:08:16 -0400

    Learning anotations in classes

---
+++ 29.Anotations/classanotation.py
@@ -0,0 +1,17 @@
+class Employee:
+    name: str
+    age: int
+    salary: float
+
+    def __init__(self, name: str, age: int, salary: float):
+        self.name = name 
+        self.age = age
+        self.salary = salary
+
+    def introduce(self) -> str:
+        return f"Hi, my name is {self.name}, I am {self.age}"
+    
+
+employe1 = Employee("Carlos", 30, 3500.0)
+
+print(employe1.introduce())
--- 29.Anotations/functionan.py
+++ 29.Anotations/functionan.py
@@ -4,3 +4,4 @@ def sum_ids(id1: int, id2: int) -> int:
     return id1 + id2
     
 print(sum_ids(201,202))
+
