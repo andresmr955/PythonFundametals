89ce8549e60af95a83802ef9a531f1f3c2416554
Andres Marquez <andresmr955@gmail.com>
2025-03-28 18:34:47 -0400

    Practicing closures and variables

--- 28.Scope_y_closures/inner_outer.py
+++ 28.Scope_y_closures/inner_outer.py
@@ -14,3 +14,12 @@ def outer_function():
 
 outer_function()
 print(f'Here we print x {x} outside outer function but indented')
+
+
+def make_adder(x):
+    def adder(y):
+        return x + y
+    return adder
+
+add_10 = make_adder(10)  # `add_10` es ahora una función que agrega 10
+print(add_10(5))  # Imprime 15 porque 10 + 5 =
---
+++ 28.Scope_y_closures/scope.py
@@ -0,0 +1,31 @@
+"""
+2 Funciones
+
+La primera funcion recibira un diccionario que tenga 3 claves
+La segunda funcion debera analizar el salario y  devolver el 
+salario de los empleados que ganen cierto monto
+"""
+
+first_dictionary = [
+    
+    {
+    "name" : "Carlos",
+    "age" : "25", 
+    "salary" : 4500
+    }, 
+    {
+    "name" : "Camilo",
+    "age" : "75", 
+    "salary" : 8500
+    },
+    {
+    "name" : "Sandra",
+    "age" : "17", 
+    "salary" : 3600
+    }
+]
+
+def receive_dic(x, y, z):
+    pass
+
+receive_dic(first_dictionary)
