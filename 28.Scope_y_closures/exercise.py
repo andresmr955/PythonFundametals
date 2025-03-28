5117860df8dcccded0a01add1d6a869f8811454a
Andres Marquez <andresmr955@gmail.com>
2025-03-28 12:55:13 -0400

    Learning anotations

---
+++ 28.Scope_y_closures/exercise.py
@@ -0,0 +1,23 @@
+##Global variable
+my_dict = [
+    {
+        "name": "Jose",
+        "age": 27,
+        "salary": 100.000
+    }, 
+    {
+        "name": "camilo", 
+        "age": 28,
+        "salary": 300.000
+    }
+] 
+
+def receive_dic(dict_x):
+    print(dict_x)
+    y = dict_x
+    def return_salary(y):
+        solution = [ ]
+        return solution
+    return_salary(y)    
+
+receive_dic(my_dict)
---
+++ 29.Anotations/anotations.py
@@ -0,0 +1,11 @@
+
+##IDS employees
+id1: int = 101
+id2: int = 102
+
+
+#Sum IDS
+total_id: int = id1 + id2
+# show the total
+print(total_id)
+
