c494ec34928f2ca2588b42a04d55f42b5cc5b2e0
Andres Marquez <andresmr955@gmail.com>
2025-03-31 09:01:10 -0400

    modifying crud

---
+++ PYTHONZEN/03_CRUD_MODIFYING.py
@@ -0,0 +1,32 @@
+set_countries = {"Colombia", "Mexico", "Bolivia","Colombia"} 
+
+size = len(set_countries)
+print(size)
+
+print('Colombia' in set_countries)
+print("Peru" in set_countries)
+
+
+# add 
+set_countries.add('Peru')
+print(set_countries)
+set_countries.add('Peru')
+print(set_countries)
+
+#update
+set_countries.update({'argentina', 'ecu', 'peru'})
+print(set_countries)
+
+## Remove
+set_countries.remove('Colombia')
+print(set_countries)
+
+
+set_countries.remove('Colom')
+print(set_countries)
+
+# try to remove without crash
+
+set_countries.discard('ar')
+print(set_countries)
+
