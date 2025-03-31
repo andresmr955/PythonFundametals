cb43b75953bfd474c1bfc0ac6c3a7dc0437d53cb
Andres Marquez <andresmr955@gmail.com>
2025-03-31 08:09:00 -0400

    Python zen and sets

---
+++ PYTHONZEN/02_sets.py
@@ -0,0 +1,22 @@
+set_countries = {"Colombia", "Mexico", "Bolivia","Colombia"} 
+print(set_countries)
+print(type(set_countries))
+
+set_numbers = {1, 2, 3, 4, 5, 6, 6}
+print(set_numbers)
+
+set_types = {1, "Hola", False, 12.12}
+print(set_types)
+
+set_from_string = set("Hoola")
+print(set_from_string)
+
+set_from_tuples = set(('abc', 'cbs', 'as', 'abc'))
+print(set_from_tuples)
+
+numbers = [1, 2, 3, 1, 2, 3, 4 ]
+set_numbers = set(numbers)
+print(set_numbers)
+
+##How to get the real order
+unique_numbers = list(set_numbers)
