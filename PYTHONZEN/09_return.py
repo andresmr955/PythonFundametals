f1190d4dd209aadc2b0a18b908a8457dd5c0d0a3
Andres Marquez <andresmr955@gmail.com>
2025-04-02 08:05:48 -0400

    retun function

---
+++ PYTHONZEN/09_return.py
@@ -0,0 +1,9 @@
+def return_fun(min, max):
+    sum = 0
+    for z in range(min, max):
+        sum+= z 
+    return sum
+
+##Take in mind to show the result is mandatory to use the print
+##But if you need just the return il will be store
+print(return_fun(1,10))
