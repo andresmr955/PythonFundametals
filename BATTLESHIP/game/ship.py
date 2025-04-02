0f866a80818ad66fa40575da10f33cc5acf98654
Andres Marquez <andresmr955@gmail.com>
2025-04-02 18:19:00 -0400

    We created the player class and we add methods and we ask to the customer the row and column

--- BATTLESHIP/game/ship.py
+++ BATTLESHIP/game/ship.py
@@ -7,11 +7,11 @@ class Ship:
         self.positions = []
         self.hits = 0
 
-    def place_ship(self, board):
+    def place_ship(self, board, start_row, start_col, direction):
         
-        self.start_row = int(input("Ingresa la fila inicial: =>"))
-        self.start_col = int(input("Ingresa la columna inicial: =>"))
-        self.direction = input("Direction del varco: (H): Horinzontal o (V): Vertical: ")
+        self.start_row = start_row
+        self.start_col = start_col
+        self.direction = direction
         print(f'{self.start_row}, {self.start_col}, {self.direction.upper()}')
         
         if self.direction.upper() == "H":
@@ -29,7 +29,8 @@ class Ship:
                         for i in range(self.size):
                             board[self.start_row][self.start_col + i ] = self.name
                             self.positions.append((self.start_row, self.start_col  + i))
-                        print(f'The ships position is from ({self.start_row}, {self.start_col}) to ({self.start_row}, {self.start_col + self.size - 1}) )')
+                        print(f'The ships position is from ({self.start_row}, {self.start_col}) to ({self.start_row}, {self.start_col + self.size - 1})')
+                        return True
             else: 
                 print("The ship's size does not fit, please enter a correct size")
                 sys.exit()
@@ -49,7 +50,7 @@ class Ship:
                         for i in range(self.size):
                             board[self.start_row + i][self.start_col] = self.name
                             self.positions.append((self.start_row + i, self.start_col))
-                            print(f'The ships position is ({self.start_row}, {self.start_col})')
+                        print(f'The ships position is ({self.start_row}, {self.start_col}) to ({self.start_row + self.size -1}, {self.start_col})')
             else: 
                 print("The ship's size does not fit, please enter a correct size")
                 sys.exit()
@@ -72,15 +73,61 @@ class Ship:
 class Destroyer(Ship): 
     def __init__(self):
         ##We have to call the constructor of parent class
-        super().__init__("Destroyer", 2)
+        super().__init__("D", 2)
 
 class Submarine(Ship):
     def __init__(self):
-        super().__init__("Submarine", 3)
+        super().__init__("S", 3)
                 
 class Battleship(Ship):
     def __init__(self):
-        super().__init__("BattleShip", 4)
+        super().__init__("B", 4)
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.board = [[" " for _ in range(10)] for _ in range(10)]
+
+
+    def place_ships(self):
+        destroyer = Destroyer()
+        submarine = Submarine()
+        battleship = Battleship()
+
+        while not destroyer.place_ship(self.board, start_row= int(input("Ingresa la fila inicial del destroyer: =>")), 
+                                start_col= int(input("Ingresa la columna inicial del destroyer: =>")), 
+                                direction= input("Direction del destroyer: (H): Horinzontal o (V): Vertical: ")):
+            
+            print("Try again for the Destroyer.")
+        while not submarine.place_ship(self.board, start_row= int(input("Ingresa la fila inicial del submarino: =>")), 
+                             start_col= int(input("Ingresa la columna inicial del submarino: =>")), 
+                             direction= input("Direction del submarino: (H): Horinzontal o (V): Vertical: ")):
+            
+            print("Try again for the submarine.")
+
+        while not battleship.place_ship(self.board, start_row= int(input("Ingresa la fila inicial del battleship: =>")), 
+                              start_col= int(input("Ingresa la columna inicial del battleship: =>")), 
+                              direction= input("Direction del battleship: (H): Horinzontal o (V): Vertical: ")):
+            print("Try again for the battleship.")
+
+
+    def print_board(self):
+        for fila in self.board:
+            print(" | " .join(fila))
+
+player = Player("Andres")
+print('*' * 40)
+print("Welcome to Battle Ship")
+print(f"The players name is {player.name}")
+print('*' * 40)
+
+player.place_ships()
+
+
+print('*' * 40)
+print("After placing ships:")
+player.print_board()
 
 '''
 board = [
@@ -97,13 +144,7 @@ board = [
 '''
 ##change hot fix
 
-board = [[ " " for _ in range(5) ] for _ in range(8) ]
+#board = [[ " " for _ in range(6) ] for _ in range(9) ]
 
-ship1 = Ship("S", 4)
-ship1.place_ship(board)
+#ship1 = Ship("S", 4)
 
-ship1.hit()
-for fila in board:
-    print(" | " .join(fila))
-
-    
