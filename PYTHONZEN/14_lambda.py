ff5c40d04b4064e5804049f584a4618d05e4dff4
Andres Marquez <andresmr955@gmail.com>
2025-04-02 13:27:49 -0400

    learning lambda 2.0

---
+++ PYTHONZEN/14_lambda.py
@@ -0,0 +1,13 @@
+def increment(x):
+    return x + 1
+result = increment(10)
+
+print(11)
+
+
+
+solution = lambda x: x + 1
+print(solution(20))
+
+full_name = lambda name, last_name: f'Your name is {name.title()} {last_name.title()}'
+print(full_name("andres", "david"))
