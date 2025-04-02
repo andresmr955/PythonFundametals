b59acd465277e0d8497cb08781507245748678a0
Andres Marquez <andresmr955@gmail.com>
2025-04-02 18:34:08 -0400

    we separate player from ship

--- BATTLESHIP/game/ship.py
+++ BATTLESHIP/game/ship.py
@@ -84,50 +84,6 @@ class Battleship(Ship):
         super().__init__("B", 4)
 
 
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.board = [[" " for _ in range(10)] for _ in range(10)]
-
-
-    def place_ships(self):
-        destroyer = Destroyer()
-        submarine = Submarine()
-        battleship = Battleship()
-
-        while not destroyer.place_ship(self.board, start_row= int(input("Ingresa la fila inicial del destroyer: =>")), 
-                                start_col= int(input("Ingresa la columna inicial del destroyer: =>")), 
-                                direction= input("Direction del destroyer: (H): Horinzontal o (V): Vertical: ")):
-            
-            print("Try again for the Destroyer.")
-        while not submarine.place_ship(self.board, start_row= int(input("Ingresa la fila inicial del submarino: =>")), 
-                             start_col= int(input("Ingresa la columna inicial del submarino: =>")), 
-                             direction= input("Direction del submarino: (H): Horinzontal o (V): Vertical: ")):
-            
-            print("Try again for the submarine.")
-
-        while not battleship.place_ship(self.board, start_row= int(input("Ingresa la fila inicial del battleship: =>")), 
-                              start_col= int(input("Ingresa la columna inicial del battleship: =>")), 
-                              direction= input("Direction del battleship: (H): Horinzontal o (V): Vertical: ")):
-            print("Try again for the battleship.")
-
-
-    def print_board(self):
-        for fila in self.board:
-            print(" | " .join(fila))
-
-player = Player("Andres")
-print('*' * 40)
-print("Welcome to Battle Ship")
-print(f"The players name is {player.name}")
-print('*' * 40)
-
-player.place_ships()
-
-
-print('*' * 40)
-print("After placing ships:")
-player.print_board()
 
 '''
 board = [
