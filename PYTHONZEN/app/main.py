7d7b2db23b20dc72ff4f1911e33f34700ee78d96
Andres Marquez <andresmr955@gmail.com>
2025-04-04 10:47:20 -0400

    We created the first module

--- PYTHONZEN/app/main.py
+++ PYTHONZEN/app/main.py
@@ -1,5 +1,17 @@
-import mod
+import utils
 
-keys, values =mod.get_population()
+keys, values =utils.get_population()
 
 print(keys, values)
+
+list_x = [
+    {'Country': 'USA', 'Population': 331002651},
+    {'Country': 'Canada', 'Population': 37742154},
+    {'Country': 'Mexico', 'Population': 128932753},
+    {'Country': 'Brazil', 'Population': 212559417},
+    {'Country': 'Argentina', 'Population': 45195777},
+]
+
+country = input("Enter the country name: ")
+solution = utils.population_by_country(list_x, country)
+print(solution)
