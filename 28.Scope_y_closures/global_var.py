2e19a1d52a3f0481042afb03133f85f26bd82471
Andres Marquez <andresmr955@gmail.com>
2025-04-04 16:17:13 -0400

    Practicing modules and package

--- 28.Scope_y_closures/global_var.py
+++ 28.Scope_y_closures/global_var.py
@@ -1,8 +1,15 @@
 x = 5
+y = 9
+print(id(x))
+print(id(y))
 
 def modify_global():
     ##This line makes reference to say to the function that x is not local is global
     global x 
+    y = 16
+    print(id(y))
+    print(id(x))
+
     x+=3
     print(f'Value modified: {x}')
 
---
+++ PYTHONZEN/modules/package/mod_1.py
@@ -0,0 +1,5 @@
+def funct_1():
+    return "Function 1 from mod_1"
+
+def funct_2():
+    return "Function 2 from mod_1"
