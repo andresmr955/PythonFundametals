30a32cd7211f3d397ccad0f322a1b978129c96b5
Andres Marquez <andresmr955@gmail.com>
2025-04-08 11:39:18 -0400

    Chanlange solved with pie and matplolib

--- PYTHONZEN/charts/charts.py
+++ PYTHONZEN/charts/charts.py
@@ -12,11 +12,12 @@ def generate_pie(labels, values):
     
 
     fig, ax = plt.subplots()
-    ax.pie(values, labels=labels)
+    ax.pie(values, labels=labels, autopct='%1.1f%%')
     ax.axis('equal')
+    plt.tight_layout()
     plt.show()
     
 if __name__ == '__main__':
     labels = ['a', 'b', 'c']
     values = [100,200, 80 ]
-    generate_bar_chart(labels, values)
+    generate_pie(labels, values)
