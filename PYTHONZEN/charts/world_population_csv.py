cdfc8787c897e78bf746d21d49bae6a22d017092
Andres Marquez <andresmr955@gmail.com>
2025-04-08 10:16:37 -0400

    Practicing how to improve and make a graphic with a real csv

---
+++ PYTHONZEN/charts/world_population_csv.py
@@ -0,0 +1,45 @@
+import csv
+import matplotlib.pyplot as plt
+
+def read_csv(path, country_user):
+    with open(path, mode='r+') as file:
+        reader = csv.DictReader(file)
+        
+        
+        for row in reader:
+            #print('***' * 6)
+            #print(row['Country/Territory'])
+            if country_user == row['Country/Territory']:
+                #'country': row['Country/Territory'],
+                print(f'Your country is {country_user}')
+                # Lista de claves y valores
+                labels = ['2022 Population', '2020 Population', '2015 Population', '2010 Population', '2000 Population', '1990 Population', '1980 Population', '1970 Population']
+                values = [int(row[label]) for label in labels]
+
+                #crate a dictionary comprehension
+
+                country_dict = {label: values for label, values in zip(labels, values)}
+                print('******' * 6)
+                print(country_dict)
+                print('******' * 6)
+                generate_bar_chart(labels, values)
+                
+def generate_bar_chart(labels, values):
+    #create the figure and eje
+    fig, ax = plt.subplots()
+    #create graphic and barras
+    ax.bar(labels, values)
+
+    # Rotar las etiquetas del eje X para mejorar la legibilidad
+    plt.xticks(rotation=45, ha='right')
+
+    plt.show()
+
+
+original_file = './world_population.csv'
+country_user = input("Which country do you want to se the population?")
+
+title_case = country_user.title()
+if __name__ == '__main__':
+    read_csv(original_file, title_case)
+    
