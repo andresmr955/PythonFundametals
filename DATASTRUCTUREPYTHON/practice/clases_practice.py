067e5e6168c9fd1f611e21cb568e38815ea1a508
Andres Marquez <andresmr955@gmail.com>
2025-04-09 14:18:24 -0400

    Level 2: this is a book class

--- DATASTRUCTUREPYTHON/practice/clases_practice.py
+++ DATASTRUCTUREPYTHON/practice/clases_practice.py
@@ -98,3 +98,24 @@ my_student = Student("Andres David")
 my_student.grades = [40, 30, 60, 80, 90]
 print(my_student.grades)
 my_student.calculate_average()
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.read = False
+    
+    def mark_read(self):
+        self.read = True
+        print(f'You have marked {self.title} as read')
+
+    def show_status(self):
+        if self.read:
+            print(f'{self.title} by {self.author} has already been read')
+        else:
+            print(f'{self.title} by {self.author} has not been read yet')
+
+my_book = Book("The little prince", "Andres")
+my_book.mark_read()
+
+
