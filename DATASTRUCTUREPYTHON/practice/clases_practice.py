fa2bb67a3e4fbb14722c4a92f961c88b1a2b5d90
Andres Marquez <andresmr955@gmail.com>
2025-04-09 13:23:10 -0400

    create 2 class with rectangle

--- DATASTRUCTUREPYTHON/practice/clases_practice.py
+++ DATASTRUCTUREPYTHON/practice/clases_practice.py
@@ -8,3 +8,18 @@ class Persona:
 person = Persona("Andres", 26)
 person.greet()
 
+class Rectangle():
+    def __init__(self, weight, hight):
+        self.weight = weight
+        self.hight = hight
+    
+    def calculate_area(self):
+        return self.weight * self.hight
+    
+    def perimeter(self):
+        total = 2 * (self.weight + self.hight)
+        return total
+    
+rectangle = Rectangle(20, 10)
+print(rectangle.perimeter())
+print(rectangle.calculate_area())
