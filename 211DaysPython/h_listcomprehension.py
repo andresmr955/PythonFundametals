8cd46243d266a298f3fd9fb2fe8a2a6c553701ab
Andres Marquez <andresmr955@gmail.com>
2025-04-10 21:25:53 -0400

    finding 2 vowels with filter and lambda

--- 211DaysPython/h_listcomprehension.py
+++ 211DaysPython/h_listcomprehension.py
@@ -13,3 +13,8 @@ def find_words_with_two_vowels(words):
     return solution
 
 print(find_words_with_two_vowels(user_input))
+
+def find_vowels_filter(words):
+    vowels = "aeiouAEIOU"
+    return list(filter(lambda word: sum(word.count(vowel) for vowel in vowels) == 2, words))
+print(find_vowels_filter(user_input))
