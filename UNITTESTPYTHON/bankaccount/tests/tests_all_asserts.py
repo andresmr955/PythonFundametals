d45b6d0c1c6d09727b0179dcb0418e04ff7e682b
Andres Marquez <andresmr955@gmail.com>
2025-04-24 15:45:57 -0400

    learning about decorators in unittest

--- UNITTESTPYTHON/bankaccount/tests/tests_all_asserts.py
+++ UNITTESTPYTHON/bankaccount/tests/tests_all_asserts.py
@@ -1,5 +1,6 @@
 import unittest
 
+SERVER = "server_b"
 class AllAssertsTests(unittest.TestCase):
     def test_assert_equal(self):
         self.assertEqual(10, 10)
@@ -29,3 +30,15 @@ class AllAssertsTests(unittest.TestCase):
     def test_assert_set(self):
         user = { 1,2,3}
         self.assertSetEqual({ 1,2,3}, user)
+    
+    @unittest.skip("Work in progress, we will enable soon")
+    def test_skip(self):
+        self.assertEqual("Hola", "bye")
+
+    @unittest.skipIf(SERVER == "server_b", "Skipped because we are not in the server")
+    def test_skip_if(self):
+        self.assertEqual(100, 100)
+
+    @unittest.expectedFailure
+    def test_expected_failure(self):
+        self.assertEqual(100 == 150)
