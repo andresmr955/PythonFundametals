319211404a60d4a05abc5635592b4e8a65bb8b20
Andres Marquez <andresmr955@gmail.com>
2025-04-28 07:53:59 -0400

    learning how to use deque

--- 31.EstructurasAvanzadas/collectionsclass.py
+++ 31.EstructurasAvanzadas/collectionsclass.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 def count_products(orders: list[str]) -> defaultdict:
     #Create a dictionary with a value 0 by default
@@ -10,3 +10,13 @@ def count_products(orders: list[str]) -> defaultdict:
 orders = ['laptop', 'smartphone', 'laptop', 'tablet']
 result = count_products(orders)
 print(result)  # Output: defaultdict(<class 'int'>, {'laptop': 2, 'smartphone': 1, 'tablet': 1})
+
+#EXAMPLE 2 
+
+def count_products_counter(products: list[str]) -> Counter:
+    #Create a dictionary with a value 0 by default
+    return Counter(products)
+
+orders = ['laptop', 'smartphone', 'laptop', 'tablet']
+result_2 = count_products_counter(orders)
+print(result_2)  # Output: defaultdict(<class 'int'>, {'laptop': 2, 'smartphone': 1, 'tablet': 1})
