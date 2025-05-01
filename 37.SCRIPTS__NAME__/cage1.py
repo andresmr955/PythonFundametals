808ff10ccfc06c5f803a56b9889d0a575f7ed508
Andres Marquez <andresmr955@gmail.com>
2025-05-01 14:41:59 -0400

    print change

--- 37.SCRIPTS__NAME__/cage1.py
+++ 37.SCRIPTS__NAME__/cage1.py
@@ -1,4 +1,5 @@
-# cage.py
+# cage.py 
+print(__name__)
 
 def dog1():
     print("Dog 1 is ready to go for a run!")
@@ -10,7 +11,9 @@ dog2()
 def dog3():
     print("Dog 3 is ready to go for a run!")
 
+
 # Leash: dog3 only runs if you execute the file directly
 if __name__ == "__main__":
     print("This is cage.py and it is being executed directly.")
     dog1()
+
--- 37.SCRIPTS__NAME__/cage2.py
+++ 37.SCRIPTS__NAME__/cage2.py
@@ -1,5 +1,5 @@
 # cage2.py
 import cage1  # Import the cage1 file
-
+print(__name__)
 print("This is cage2.")
 cage1.dog3()  # Call dog3 from cage1
