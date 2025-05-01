59d1fb0a854d6ac5c3d26fce687788c93cfb2280
Andres Marquez <andresmr955@gmail.com>
2025-05-01 14:35:54 -0400

    making the homework

---
+++ 37.SCRIPTS__NAME__/homeworkimported.py
@@ -0,0 +1,17 @@
+import homework
+empleados = [
+    "Ana Torres",
+    "Luis Ramírez",
+    "María Gómez",
+    "Carlos Díaz",
+    "Laura Pérez",
+    "José Herrera",
+    "Paula Ríos",
+    "Diego Castro",
+    "Sofía Medina",
+    "Ricardo Molina"
+]
+
+
+result = homework.delete_employees(empleados, "Ana Torres" )
+print(f'This is the result from imported module {result}')
