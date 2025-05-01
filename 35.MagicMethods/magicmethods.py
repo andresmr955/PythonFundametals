9249318f7c900a6174dff0cddb953a0ae5177432
Andres Marquez <andresmr955@gmail.com>
2025-05-01 08:26:18 -0400

    equality comparison

--- 35.MagicMethods/magicmethods.py
+++ 35.MagicMethods/magicmethods.py
@@ -32,13 +32,15 @@ class Toy:
     def __add__(self, other_toy):
         # Corrected: Now we pass both arguments, name and model, to the constructor
         return Toy(self.name + " and " + other_toy.name, self.model + other_toy.model)
-
+    
+    def __eq__(self, other):
+        return self.name == other.name and self.age == other.age
 # Create two toys
 toy1 = Toy("Car", 2000)
 toy2 = Toy("Airplane", 1989)
 
 # Use the magic method __add__ to "add" the toys
 toy3 = toy1 + toy2
-
+print(f'__eq__: => {toy1.__eq__(toy2)}')
 print(toy3.name)
 print(toy3.model)
