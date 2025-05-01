e295d6d6bb5d22f4a631f7c5d92a9407f2b06c5b
Andres Marquez <andresmr955@gmail.com>
2025-05-01 09:09:09 -0400

    practicing overloading operators with a vector

--- 35.MagicMethods/magicmethods.py
+++ 35.MagicMethods/magicmethods.py
@@ -1,18 +1,18 @@
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
-    def __str__(self):
+    def __str__(self) -> str:
         return f'Person: {self.name}, {self.age} years old'
 
 person = Person("Andres", 26)
 print(person)
 
 class Toy:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
 
-    def __add__(self, other_toy):
+    def __add__(self, other_toy) -> str:
         return Toy(self.name + " and " + other_toy.name)
 
 # Create two toys
@@ -25,16 +25,20 @@ toy3 = toy1 + toy2
 print(toy3.name)  # Output: Car and Airplane
 
 class Toy:
-    def __init__(self, name, model):
+    def __init__(self, name: str, model: int):
         self.name = name
         self.model = model
-
-    def __add__(self, other_toy):
+        
+    def __add__(self, other_toy) -> str:
         # Corrected: Now we pass both arguments, name and model, to the constructor
         return Toy(self.name + " and " + other_toy.name, self.model + other_toy.model)
     
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return self.name == other.name and self.age == other.age
+    
+    def __it__(self, other_person) -> bool:
+        return self.model < other_person.model
+
 # Create two toys
 toy1 = Toy("Car", 2000)
 toy2 = Toy("Airplane", 1989)
---
+++ 36.Operator Overloading in Python/vector.py
@@ -0,0 +1,17 @@
+class Vector:
+    def __init__(self, x , y):
+        self.x = x 
+        self.y = y
+    
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+    
+    def __add__(self, other_vector):
+        return Vector(self.x + other_vector.x, self.y + other_vector.y)
+    def __repr__(self):
+        return f'Vector: (X {self.x}, Y {self.y})'
+    
+vector1 = Vector(1,2)
+vector2 = Vector(2,1)
+vector3 = vector1 + vector2
+print(vector3)
