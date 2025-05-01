e18e7c59ae1884da69308151f168db5142b42d24
Andres Marquez <andresmr955@gmail.com>
2025-05-01 08:22:24 -0400

    learning add magic method with int

--- 35.MagicMethods/magicmethods.py
+++ 35.MagicMethods/magicmethods.py
@@ -23,3 +23,22 @@ toy2 = Toy("Airplane")
 toy3 = toy1 + toy2
 
 print(toy3.name)  # Output: Car and Airplane
+
+class Toy:
+    def __init__(self, name, model):
+        self.name = name
+        self.model = model
+
+    def __add__(self, other_toy):
+        # Corrected: Now we pass both arguments, name and model, to the constructor
+        return Toy(self.name + " and " + other_toy.name, self.model + other_toy.model)
+
+# Create two toys
+toy1 = Toy("Car", 2000)
+toy2 = Toy("Airplane", 1989)
+
+# Use the magic method __add__ to "add" the toys
+toy3 = toy1 + toy2
+
+print(toy3.name)
+print(toy3.model)
