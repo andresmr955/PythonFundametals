631a4b3efcdfff270f68dc09246b92af87cf8059
Andres Marquez <andresmr955@gmail.com>
2025-05-06 17:36:09 -0400

    creating unit test

---
+++ 40.Privatesandprotected/exercise/test_bankaccount.py
@@ -0,0 +1,32 @@
+import unittest
+import os
+from classbank import BankAccount
+
+
+class TestBankAccount(unittest.TestCase):
+    def setUp(self):
+        print("Setting up the test case...")
+        self.account = BankAccount(1000.0, "123456789")
+
+    def tearDown(self):
+        print("Tearing down the test case...   cleaning up...")
+        if os.path.exists('40.Privatesandprotected/exercise/transactions.txt'):
+            os.remove('40.Privatesandprotected/exercise/transactions.txt')
+
+    def test_initial_balance(self):
+        self.assertEqual(self.account.get_balance, 1000.0)
+    
+    def test_deposit(self):
+        self.account.deposit(500)
+        self.assertEqual(self.account.get_balance, 1500)
+
+    def test_withdraw_balance(self):
+        self.account.withdraw_balance(600)
+        self.assertEqual(self.account.get_balance, 400)
+
+    def test_transaction_file_created(self):
+        self.account.deposit(100) 
+        self.assertTrue(os.path.exists('40.Privatesandprotected/exercise/transactions.txt'))
+
+if __name__ == "__main__":
+    unittest.main()
