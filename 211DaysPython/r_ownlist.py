ceb55bdb2e0353956737564773dd63ce97632f79
Andres Marquez <andresmr955@gmail.com>
2025-05-07 16:33:57 -0400

    creating a list

---
+++ 211DaysPython/r_ownlist.py
@@ -0,0 +1,24 @@
+
+class MyList:
+    def __init__(self, initial_data= None):
+        # Tu código aquí 👇
+        self.data = initial_data if initial_data else []
+        self.length = 0
+
+my_o_List = MyList()
+
+my_o_List.append(1)
+my_o_List.append(2)
+my_o_List.append(3)
+my_o_List.append(4)
+my_o_List.append(5)
+
+print([i for i in my_o_List])
+#my_o_List.append(3)
+
+
+#new_list = my_o_List.map(lambda x: x * 2)
+#print(new_list)
+
+print(my_o_List.filter(lambda x: x > 2))
+print(my_o_List.join(character="x"))
