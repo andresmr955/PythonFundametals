4a87f1dab45f50c9395c2fe6af3bdc7ccaaefed0
Andres Marquez <andresmr955@gmail.com>
2025-05-14 15:15:27 -0400

    side effects

--- UNITTESTPYTHON/bankaccount/tests/test_api_client.py
+++ UNITTESTPYTHON/bankaccount/tests/test_api_client.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, requests
 from unittest.mock import patch
 from src.exchange_api import get_location
 
@@ -6,6 +6,7 @@ class ApiClientTests(unittest.TestCase):
     
     @patch('src.exchange_api.requests.get')
     def test_get_location_returns_expected_area(self, mock_get):
+
         mock_get.return_value.status_code = 200
         mock_get.return_value.json.return_value = {
             "countryName" : "United States of America",
@@ -13,8 +14,33 @@ class ApiClientTests(unittest.TestCase):
             "cityName" : "MIAMI",
         
         }
+        
         result = get_location("8.8.8.8")
         self.assertEqual(result.get("country"), "United States of America")
         self.assertEqual(result.get("region"), "FLORIDA")
         self.assertEqual(result.get("city"), "MIAMI")
         mock_get.assert_called_once_with("https://freeipapi.com/api/json/8.8.8.8")
+
+    @patch('src.exchange_api.requests.get')
+    def test_get_location_returns_side_effect(self, mock_get):
+        mock_get.side_effect = [requests.exceptions.RequestException("Service Unavailable"), unittest.mock.Mock(
+            status_code = 200,
+            json=lambda: {
+                "countryName" : "United States of America",
+                "regionName" : "FLORIDA",
+                "cityName" : "MIAMI",
+        
+            }
+        )]
+
+  
+        
+        with self.assertRaises(requests.exceptions.RequestException):
+            get_location("8.8.8.8")
+         
+        result = get_location("8.8.8.8")
+        self.assertEqual(result.get("country"), "United States of America")
+        self.assertEqual(result.get("region"), "FLORIDA")
+        self.assertEqual(result.get("city"), "MIAMI")
+     
+        # result = get_location("8.8.8.8")
