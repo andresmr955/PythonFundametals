42dc450ab79c0bc2a3db2e0b216560f59d06bfa3
Andres Marquez <andresmr955@gmail.com>
2025-05-17 22:06:55 -0400

    creating a graphic of iterations

--- ALGORITHMICCOMPLEXITY/graphspandas.py
+++ ALGORITHMICCOMPLEXITY/graphspandas.py
@@ -5,18 +5,30 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('time-complexity.csv')
 print(df.head)
 
-
+fig, (ax1, ax2) = plt.subplots(2,1, figsize=(10,8), gridspec_kw={"height_ratios": [3,1]})
 #Graphs
 
-plt.figure(figsize=(10,6))
-plt.plot(df['n'], df['LOOP'], marker='o', label='LOOP')
-plt.plot(df['n'], df['Recursivity'], marker='s', label='Recursivity')
-plt.xlabel('n (Quantity of iterations)')
-plt.ylabel('Time Seconds')
-plt.title('Comparison of times: Loop vs Recursivity')
-plt.legend()
-plt.grid(True)
+ax1.plot(df['n'], df['LOOP'], marker='o', label='LOOP')
+ax1.plot(df['n'], df['Recursivity'], marker='s', label='Recursivity')
+ax1.set_xlabel('n (Quantity of iterations)')
+ax1.set_ylabel('Time Seconds')
+ax1.set_title('Comparison of times: Loop vs Recursivity')
+ax1.legend()
+ax1.grid(True)
+
+
+ax2.axis('off')
+
+#table
+
+table = ax2.table(
+    cellText=df.round(6).values,
+    colLabels=df.columns,
+    loc='center'
+)
+
+
+table.scale(1, 1.5)
 plt.tight_layout()
 plt.savefig('graphtimesimg.png')
-
 plt.show()
