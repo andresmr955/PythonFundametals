30b2fbf12f5f9ec80fe660102521b4b99b35dcab
Andres Marquez <andresmr955@gmail.com>
2025-05-19 08:01:31 -0400

    Doctest

--- UNITTESTPYTHON/calculator/src/calculator.py
+++ UNITTESTPYTHON/calculator/src/calculator.py
@@ -1,11 +1,27 @@
 # from math import inf
 def sum(a, b):
+
+    """ 
+    >>> sum(5,7)
+    12
+    
+    
+    >>> sum(5,10)
+    15
+    """
     return a + b
 
+   
 def subtract(a, b):
     return a - b
 
 def multiply(a, b):
+    """
+    >>> multiply(3,3)
+    10
+
+    """
+    
     return a * b
 # def divide(a,b):
 #     if b != 0:
@@ -19,7 +35,20 @@ def multiply(a, b):
     
 
 def divide(a,b):
+
+    """
+
+    >>> divide(15,5)
+    3.0
+    >>> divide(10, 0)
+    Traceback (most recent call last): 
+    ZeroDivisionError: It is not possible to divide by zero
+    """
     if b == 0:
         raise ZeroDivisionError("It is not possible to divide by zero")
     else:
-        return a / b    
+        return a / b  
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
