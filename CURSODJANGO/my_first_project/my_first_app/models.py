859337fa9588e8021750b3584a345e40cbca4272
Andres Marquez <andresmr955@gmail.com>
2025-05-22 10:49:43 -0400

    creating one to many and learning more about Django

--- CURSODJANGO/my_first_project/my_first_app/models.py
+++ CURSODJANGO/my_first_project/my_first_app/models.py
@@ -2,4 +2,25 @@ from django.db import models
 
 # Create your models here.
 class Car(models.Model):
-    title = models.TextField(max_length=250)
+    title = models.TextField(max_length=250,null=True)
+    year = models.TextField(max_length=4, null=True)
+    color = models.TextField(max_length=50, null=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.year} - {self.color}"
+    
+
+class Publisher(models.Model):
+    name = models.TextField(max_length=200)
+    address = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.TextField(max_length=200)
+    publication_date = models.DateField()
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
