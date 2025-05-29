2baa4744bc28a4651271e7a964c61d6b8f87607c
Andres Marquez <andresmr955@gmail.com>
2025-05-29 09:30:45 -0400

    adding views urls and templates of registration form

--- DJANGOCOURSE/Coffe_second_project/coffe_project/users/views.py
+++ DJANGOCOURSE/Coffe_second_project/coffe_project/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 # Create your views here.
+from .forms import RegisForm
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login((request, user))
+            return redirect('products/product_detail')
+    else:
+        form = RegisForm()
+    return render(request, 'registration/register.html', {'form': form})
