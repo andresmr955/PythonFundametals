0dffdae75862d1b70c79fe856874e73e4129db36
Andres Marquez <andresmr955@gmail.com>
2025-05-30 17:01:24 -0400

    creating postgress

--- DJANGOCOURSE/Coffe_second_project/coffe_project/coffe_project/settings.py
+++ DJANGOCOURSE/Coffe_second_project/coffe_project/coffe_project/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'coffe_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'coffe_project_db',
+        'USER': 'andre',
+        'PASSWORD': 'fire1602',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
