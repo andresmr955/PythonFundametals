47fca14b9d981d333f259fe2d7d684b4883e26c7
Andres Marquez <andresmr955@gmail.com>
2025-07-03 09:26:36 -0400

    wather app

--- PRACTICEEXERCISE/pyramid.py
+++ PRACTICEEXERCISE/pyramid.py
@@ -1,18 +1,36 @@
+from collections import deque
+
 counter = 8
 character ="%"
-myrows = []
+my_rows = []
 
+mi_list = deque(my_rows)
 
-def spacesPyramid(sumatoria,  counterPara):
-        return " " * (counterPara - sumatoria) + character * (2 * sumatoria - 1) + " " * (counterPara - sumatoria)
+def spacesPyramid(summation,  counterPara):
+        return " " * (counterPara - summation) + character * (2 * summation - 1) + " " * (counterPara - summation)
 
+def spacesPyramidInverse(summation,  counterPara):
+        return " " * (counterPara - summation) + character * (2 * summation - 1) + " " * (counterPara - summation)
 
+my_rows_one = [spacesPyramid(i + 1, counter) for i in range(counter)]
 
-for i in range(counter):    
-      myrows.append(spacesPyramid(i + 1, counter))
-      
-result = ""
-for line in myrows:
-    result = result + line + '\n'
+for i in range(counter): 
+     mi_list.appendleft(spacesPyramid(i + 1, counter))
 
-print(result)
+my_rows_third = [spacesPyramidInverse(i, counter) for i in range(counter, 0, -1)]
+
+result_one = "\n".join(my_rows_one)
+
+result_first_inverse_deque = "\n".join(mi_list)
+
+result_second_inverse_slice = '\n'.join(my_rows_one[::-1]) + '\n'
+
+result_third_inverse_join = "\n".join(my_rows_third)
+
+print(result_one)
+print('*' * 10  + "INVERSE WITH DEQUE" + '*' * 10)
+print(result_first_inverse_deque)
+print('*' * 10  + "INVERSE WITH SLICE" + '*' * 10)
+print(result_second_inverse_slice)
+print('*' * 10 + "INVERSE WITH for i in range( counter, 0, -1)" + '*' * 10)
+print(result_third_inverse_join)
---
+++ PRACTICEEXERCISE/weather.py
@@ -0,0 +1,20 @@
+import streamlit as st 
+import requests 
+
+st.title("🌡️ \u2600 \u2601☁️ Consult the current weather")
+
+API_KEY = "65aaa2e98882febfe49033b280f1ca62"
+
+city = st.text_input("Write a city's name")
+
+if city:
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+    answer = requests.get(url)
+    if answer.status_code == 200:
+        data = answer.json()
+        st.subheader(f"The weather in {city.title()}")
+        st.write(f"\U0001F31E U+2601 Temperature: {data['main']['temp']} C")
+        st.write(f"\U0001F30A Humidity: {data['main']['humidity']} %")
+        st.write(f"\U00002611 Status: {data['weather'][0]['description'].capitalize()} %")
+    else:
+        st.error("City not found")
