fcdb1adc784b06e96b5a04b25f383bb615f6cd55
Andres Marquez <andresmr955@gmail.com>
2025-07-04 08:35:28 -0400

    kelvin to celcius

--- PRACTICEEXERCISE/weather.py
+++ PRACTICEEXERCISE/weather.py
@@ -10,10 +10,11 @@ city = st.text_input("Write a city's name")
 if city:
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
     answer = requests.get(url)
+    temps_celsius = round(answer['main']['temp'] - 273.15, 2)
     if answer.status_code == 200:
         data = answer.json()
         st.subheader(f"The weather in {city.title()}")
-        st.write(f"\U0001F31E U+2601 Temperature: {data['main']['temp']} C")
+        st.write(f"\U0001F31E \U00002601 Temperature: {data['main']['temp']} C")
         st.write(f"\U0001F30A Humidity: {data['main']['humidity']} %")
         st.write(f"\U00002611 Status: {data['weather'][0]['description'].capitalize()} %")
     else:
