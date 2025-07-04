78d2cc37357109932f61af8b683df9a684b6f74d
Andres Marquez <andresmr955@gmail.com>
2025-07-04 08:41:23 -0400

    celcius

--- PRACTICEEXERCISE/weather.py
+++ PRACTICEEXERCISE/weather.py
@@ -6,6 +6,7 @@ st.title("🌡️ \u2600 \u2601☁️ Consult the current weather")
 API_KEY = "65aaa2e98882febfe49033b280f1ca62"
 
 city = st.text_input("Write a city's name")
+unit = st.selectbox("Choose temperature unit:", ["Celsius", "Kelvin"])
 
 if city:
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
@@ -13,9 +14,18 @@ if city:
     
     if answer.status_code == 200:
         data = answer.json()
+
+        temp_kelvin = data['main']['temp']
         temps_celsius = round(data['main']['temp'] - 273.15, 2)
+       
         st.subheader(f"The weather in {city.title()}")
-        st.write(f"\U0001F31E \U00002601 Temperature: {temps_celsius} C")
+        
+        
+        if unit == "Celsius":
+            st.write(f"🌞 ☁️ Temperature: {temps_celsius} °C")
+        else:
+            st.write(f"🌞 ☁️ Temperature: {round(temp_kelvin, 2)} K")
+        
         st.write(f"\U0001F30A Humidity: {data['main']['humidity']} %")
         st.write(f"\U00002611 Status: {data['weather'][0]['description'].capitalize()} %")
     else:
